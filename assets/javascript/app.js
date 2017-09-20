74e1ea254459df630d358aed8c0d3619ca3895f7
mateyo3 <mateoreyes720@gmail.com>
2017-09-20 14:07:20 -0700

    html setp

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+//Global variables
+var correct = 0;
+	incorrect = 0;
+	unanswered = 0;
+
+var questionOne = "What is 2 + 2?"
+console.log("The question is: " + questionOne);
+
+var choices = "2"
+
+console.log("The answer is: " + choices);
+
+
+//make radio buttons
+
+
+
+
+// $("#question").html("<p>QuestionOne?<p>");
